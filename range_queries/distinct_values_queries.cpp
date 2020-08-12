1df09535eb470020c861003c562ed6b5cf6d23f6
Petar Mihalj <petar.mihalj.nasice@gmail.com>
2020-08-12 23:30:33 +0200

    fixed tle on distinct val queries, rang q complete

--- range_queries/distinct_values_queries.cpp
+++ range_queries/distinct_values_queries.cpp
@@ -38,31 +38,79 @@ int main() {
   vector<int> X(n);
   vector<query> Q(q);
 
-  int ind = 1;
-  set<int> vals;
-  unordered_map<int, int> compr;
+  vi compr;
 
   FORI(n) {
     cin >> X[i];
-    vals.insert(X[i]);
+    compr.push_back(X[i]);
   }
   FORI(q) {
     cin >> Q[i].a >> Q[i].b;
     Q[i].i = i;
     Q[i].a--;
     Q[i].b--;
-    vals.insert(Q[i].a - 1);
-    vals.insert(Q[i].b);
+  }
+  int BLOCK = 500;
+  sort(Q.begin(), Q.end(), [BLOCK](query q1, query q2) -> bool {
+    if (q1.a / BLOCK < q2.a / BLOCK)
+      return true;
+    if (q1.a / BLOCK > q2.a / BLOCK)
+      return false;
+    return q1.b < q2.b;
+  });
+
+  sort(compr.begin(), compr.end());
+  compr.erase(unique(compr.begin(), compr.end()), compr.end());
+
+  auto compf = [&compr](int i) -> int {
+    return lower_bound(compr.begin(), compr.end(), i) - compr.begin();
+  };
+
+  FORI(n) { X[i] = compf(X[i]); }
+
+  vi sett(compr.size());
+
+  vi toclear;
+
+  int lastblock = -1;
+  int nextright = -1;
+  int currentuniqcnt;
+  for (query& q : Q) {
+    if (q.a / BLOCK != lastblock) {
+      lastblock = q.a / BLOCK;
+      nextright = (lastblock + 1) * BLOCK;
+      currentuniqcnt = 0;
+      for (int i : toclear)
+        sett[i] = 0;
+      toclear.clear();
+    }
+
+    for (int i = q.a; i <= min(q.b, (lastblock + 1) * BLOCK - 1); i++) {
+      int cval = X[i];
+      if (sett[cval] == 0)
+        currentuniqcnt++;
+      sett[cval]++;
+    }
+
+    for (; nextright <= q.b; nextright++) {
+      int cval = X[nextright];
+      toclear.push_back(cval);
+      if (sett[cval] == 0)
+        currentuniqcnt++;
+      sett[cval]++;
+    }
+    q.res = currentuniqcnt;
+
+    for (int i = q.a; i <= min(q.b, (lastblock + 1) * BLOCK - 1); i++) {
+      int cval = X[i];
+      if (sett[cval] == 1)
+        currentuniqcnt--;
+      sett[cval]--;
+    }
   }
 
-  for (int i : vals)
-    compr[i] = ind++;
-
-  vi pref(compr.size() + 1);
-  FORI(n) { pref[compr[X[i]]] = 1; }
-  FORI(pref.size() - 1) pref[i + 1] += pref[i];
-
-  for (query q : Q) {
-    cout << pref[compr[q.b]] - pref[compr[q.a - 1]] << endl;
-  }
+  sort(Q.begin(), Q.end(),
+       [](query q1, query q2) -> bool { return q1.i < q2.i; });
+  for (query& q : Q)
+    cout << q.res << '\n';
 }
