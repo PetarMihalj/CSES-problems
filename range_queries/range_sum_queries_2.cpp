e19cc89549316dc47ffc8286807280713c33dcd8
Petar Mihalj <petar.mihalj.nasice@gmail.com>
2020-10-10 14:15:09 +0200

    seg

--- range_queries/range_sum_queries_2.cpp
+++ range_queries/range_sum_queries_2.cpp
@@ -17,49 +17,63 @@ typedef pair<ll, ll> llll;
 typedef vector<vll> vvll;
 typedef vector<llll> vllll;
 
-inline ll pos_mod(ll i, ll n) {
-  return (i % n + n) % n;
-}
+inline ll pos_mod(ll i, ll n) { return (i % n + n) % n; }
 #define FORI(n) for (int i = 0; i < (n); i++)
 #define FORJ(n) for (int j = 0; j < (n); j++)
 #define FORK(n) for (int k = 0; k < (n); k++)
 #define MOD 1000000007
 
-ll sum(vll& ft, int p) {
-  ll s = 0;
-  for (int i = p; i > 0; i -= i & (-i))
-    s += ft[i];
-  return s;
+vll ST;
+
+ll sum(int i, int ia, int ib, int a, int b) {
+    if (ia == a && ib == b)
+        return ST[i];
+    int im = (ia + ib) / 2;
+    if (b <= im)
+        return sum(2 * i, ia, im, a, b);
+    if (a > im)
+        return sum(2 * i + 1, im + 1, ib, a, b);
+    return sum(2 * i, ia, im, a, im) + sum(2 * i + 1, im + 1, ib, im + 1, b);
 }
 
-void add(vll& ft, int p, int dv) {
-  for (int i = p; i < ft.size(); i += i & (-i))
-    ft[i] += dv;
+void add(int i, int ia, int ib, int t, int x) {
+    if (ia == ib) {
+        ST[i] = x;
+        return;
+    }
+    int im = (ia + ib) / 2;
+    if (t <= im)
+        add(2 * i, ia, im, t, x);
+    else
+        add(2 * i + 1, im + 1, ib, t, x);
+    ST[i] = ST[2 * i] + ST[2 * i + 1];
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  int n, q;
-  cin >> n >> q;
-  vll X(n + 1);
-  vll FT(n + 1);
-  FORI(n) {
-    cin >> X[i + 1];
-    add(FT, i + 1, X[i + 1]);
-  }
-  while (q--) {
-    int t;
-    cin >> t;
-    if (t == 2) {
-      int a, b;
-      cin >> a >> b;
-      cout << sum(FT, b) - sum(FT, a - 1) << endl;
-    } else {
-      int k, u;
-      cin >> k >> u;
-      add(FT, k, u - X[k]);
-      X[k] = u;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, q;
+    cin >> n >> q;
+    ST = vll(4 * n);
+    FORI(n) {
+        int x;
+        cin >> x;
+        add(1, 0, n - 1, i, x);
     }
-  }
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 2) {
+            int a, b;
+            cin >> a >> b;
+            a--;
+            b--;
+            cout << sum(1, 0, n - 1, a, b) << endl;
+        } else {
+            int k, u;
+            cin >> k >> u;
+            k--;
+            add(1, 0, n - 1, k, u);
+        }
+    }
 }
